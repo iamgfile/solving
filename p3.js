fa8eaaf87b9cab3bb758bea0b4e35342631a8277
iamgfile <g.fail.sp@gmail.com>
2018-06-04 12:15:44 +0900

    add newton's method

--- p3.js
+++ p3.js
@@ -1,7 +1,9 @@
-const EPS = 0.0001; // constは定数の定義
+const EPS        = 0.0001; // constは定数の定義
+const ORIGINAL_A = 0.0;
+const ORIGINAL_B = 1.0;
 
 function main() {
-    var a = 0.0, b = 1.0;  // 初期値
+    var a = ORIGINAL_A, b = ORIGINAL_B;  // 初期値
 
     console.log("x^3 + x - 1 の2分法による数値計算");
     console.log("初期値 a=" + a);
@@ -12,9 +14,14 @@ function main() {
 
 function nibun(a, b) {
     var c;
-
+    var output = document.querySelector( "#output" );
     do {
         c = (a + b) / 2.0;  // 2分計算
+        console.log(c);
+        let str = document.createTextNode(c);
+        let p = document.createElement("p");
+        p.appendChild(str);
+        output.appendChild(p);
         if ((func_y(c) * func_y(a)) < 0) b = c;  // 式(1.2)
         else a = c;  // 式(1.3)
     } while (Math.abs(a - b) > EPS);  // 収束判別　式(1.4)の変形
--- p8.js
+++ p8.js
@@ -1,14 +1,20 @@
-const EPS = 0.0001;    // 許容誤差
+const EPS      = 0.0001;    // 許容誤差
+const ORIGINAL = 1.0;
 
 function main() {
     console.log("x^3 + x - 1 のニュートン法による数値計算");
 
-    var a = 1.0;
+    var a = ORIGINAL;
     console.log("初期値 a=" + a);
 
-    var b;
+    var b = ORIGINAL;
 
     // ここにニュートン法による演算の主要部分を記述する
+    do {
+        a = b;
+        b = a - func_y(a) / func_z(a);
+        console.log(b);
+    } while (Math.abs(a-b) > EPS);
 
     console.log("近似解 x = " + b);
 }
